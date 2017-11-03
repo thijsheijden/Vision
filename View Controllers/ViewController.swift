8295d665b43a432905ee91de02d4e57a89919127
Thijs van der Heijden <hi@thijsheijden.nl>
2017-11-03 21:28:35 +0100

    Added ui and linked them to viewcontroller.swift

---
+++ View Controllers/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Vision
+//
+//  Created by Thijs van der Heijden on 11/3/17.
+//  Copyright © 2017 Thijs van der Heijden. All rights reserved.
+//
+
+import UIKit
+import CoreML
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+    }
+    
+
+}
+
